120f3f492e3f08211646785f3df1642a762854a1
peiyu <1249200310@qq.com>
2022-04-03 10:34:08 +0800

    <feat>[frame]: 增加配置方案

--- main.go
+++ main.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"api/bootstrap"
+	bstConfig "api/config"
+	"api/pkg/config"
+	"flag"
 	"fmt"
 	"github.com/gin-gonic/gin"
 )
 
+func init() {
+	// 加载 config 目录下的配置信息
+	bstConfig.Initialize()
+}
+
 func main() {
+	// 配置初始化，依赖命令行 --env 参数
+	var env string
+	flag.StringVar(&env, "env", "", "加载 .env 文件，如 --env=testing 加载的是 .env.testing 文件")
+	flag.Parse()
+	config.InitConfig(env)
+
 	// 初始化 Gin 实例
-	r := gin.Default()
+	r := gin.New()
 
 	// 初始化路由绑定
 	bootstrap.SetupRoute(r)
 
-	err := r.Run(":8080")
+	// 运行服务
+	err := r.Run(":" + config.Get("app.port"))
 	if err != nil {
+		// 错误处理，端口被占用了或者其他错误
 		fmt.Println(err.Error())
 	}
 }
