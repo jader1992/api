ce9c172f5cc3356afc1a04ede96be46716a35324
peiyu <1249200310@qq.com>
2022-04-05 13:54:30 +0800

    <feat>[user]: 验证请求

--- app/http/controllers/api/v1/auth/signup_controller.go
+++ app/http/controllers/api/v1/auth/signup_controller.go
@@ -3,6 +3,7 @@ package auth
 import (
 	v1 "api/app/http/controllers/api/v1"
 	"api/app/models/user"
+	"api/app/requests"
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"net/http"
@@ -15,12 +16,8 @@ type SignupController struct {
 
 // IsPhoneExist 检测手机号是否被注册
 func (sign *SignupController) IsPhoneExist(c *gin.Context) {
-	// 请求对象
-	type PhoneExistRequest struct {
-		Phone string `json:"phone"`
-	}
-
-	request := PhoneExistRequest{}
+	// 初始化请求对象
+	request := requests.SignupPhoneExistRequest{}
 
 	// 解析JSON
 	if err := c.ShouldBind(&request); err != nil {
@@ -33,9 +30,19 @@ func (sign *SignupController) IsPhoneExist(c *gin.Context) {
 		// 出错了，中断请求
 		return
 	}
+	//fmt.Println(request)
 
-	fmt.Println(request)
+	// 表单验证
+	errs := requests.ValidateSignupPhoneExist(&request, c)
+	if len(errs) > 0 {
+		// 验证失败，返回 422 状态码和错误信息
+		c.AbortWithStatusJSON(http.StatusUnprocessableEntity, gin.H{
+			"errors": errs,
+		})
+		return
+	}
 
+	//  检查数据库并返回响应
 	c.JSON(http.StatusOK, gin.H{
 		"exist": user.IsPhoneExist(request.Phone),
 	})
---
+++ app/requests/signup_request.go
@@ -0,0 +1,35 @@
+package requests
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/thedevsaddam/govalidator"
+)
+
+type SignupPhoneExistRequest struct {
+	Phone string `json:"phone,omitempty" valid:"phone"`
+}
+
+func ValidateSignupPhoneExist(data interface{}, c *gin.Context) map[string][]string {
+	// 自定义验证规则
+	rules := govalidator.MapData{
+		"phone": []string{"required", "digits:11"},
+	}
+
+	// 自定义验证出错的提示
+	messages := govalidator.MapData{
+		"phone": []string{
+			"required:手机号码必填，参数名称phone",
+			"digits:手机号长度必须为11位的数字",
+		},
+	}
+
+	// 配置初始化
+	opts := govalidator.Options{
+		Data:          data,
+		Rules:         rules,
+		TagIdentifier: "valid",
+		Messages:      messages,
+	}
+
+	return govalidator.New(opts).ValidateStruct()
+}
